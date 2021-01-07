286541dbf60cc9127bcc1fbd99636bccac0509ee
Wil McCauley <wmccauley724@gmail.com>
2021-01-07 09:14:10 -0700

    Add patron test for it exists with attributes

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -1,2 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+
+require './lib/patron'
+
+class PatronTest < Minitest::Test
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists_with_attributes
+    assert_instance_of Patron, @patron_1
+  end
+end
