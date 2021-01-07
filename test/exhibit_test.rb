4f9e30a2307000073fe3978d6e7c0fd3b0c81fcb
Wil McCauley <wmccauley724@gmail.com>
2021-01-07 09:11:16 -0700

    Add test for attributes

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -11,5 +11,6 @@ class ExhibitTest < Minitest::Test
   def test_it_exists_with_attributes
     assert_instance_of Exhibit, @exhibit
     assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
   end
 end
