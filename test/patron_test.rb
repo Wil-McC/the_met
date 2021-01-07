fb3cf96c69e9d3e84c8e6d954811dd78cb830d8e
Wil McCauley <wmccauley724@gmail.com>
2021-01-07 09:16:09 -0700

    Add test for additional attributes

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -10,5 +10,8 @@ class PatronTest < Minitest::Test
 
   def test_it_exists_with_attributes
     assert_instance_of Patron, @patron_1
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
   end
 end
